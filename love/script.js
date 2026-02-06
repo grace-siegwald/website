6e40937df31397f3f86bcf825b7bb15478168501
grace-siegwald <siegwald@colum.edu>
2026-02-06 12:28:09 -0600

    stuff

---
+++ love/script.js
@@ -0,0 +1,11 @@
+//this is ur imagge
+const thingy = document.getElementById("GIF")
+
+// these are ur two buttons!
+const thing2 = document.getElementById("yesButton")
+
+const thing3 = document.getElementById("noButton")
+
+thing2.addEventListener("click", function(){
+    thingy.style.display = "block";
+});
